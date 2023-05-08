b0e5d928043e59562ce1dd3b8ca265b97b2cb0fd
williamtelesd <wtelesdias@gmail.com>
2023-05-08 09:53:28 -0300

    Compressao terminada(?)

--- ziplz78.py
+++ ziplz78.py
@@ -36,24 +36,66 @@ class No:
         for filho in self.filhos:
             filho.print(nivel+1)
         
+class lz78:
     
-string = ""
-trie = No(string, 0)
-trie.print()
-f = open("teste.txt")
-counter = 1
-while True:
-    char = f.read(1)
-    
-    if not char:
-        break
-    busca = trie.busca(string + char)
-    if busca != None:
-        string = string + char
-    else:
-        print([trie.busca(string), char])
-        trie.addPalavra(string + char, counter)
-        counter += 1
-        string = ''
+    def comprime(fEntrada, fSaida):
+        string = ""
+        trie = No(string, 0)
+        bytes = 0
+        cod = 1
+        counter = 0
+        # Primeira passada
+        while True:
+            char = fEntrada.read(1)
+            if not char:
+                string = ''
+                break
+            
+            busca = trie.busca(string + char)
+            if busca != None:
+                string = string + char
+            else:
+                counter += 1
+                print([trie.busca(string)])
+                trie.addPalavra(string + char, cod)
+                cod += 1
+                string = ''
 
-trie.print()
+        # Determina numero de bytes
+        if counter <= 255:
+            bytes = 1
+        elif counter <= 65535:
+            bytes = 2
+        elif counter <= 16_777_216:
+            bytes = 3
+        elif counter <= 200_000_000:
+            bytes = 4
+        
+        fEntrada.seek(0) # volta pro inicio do arquivo
+        fSaida.write(bytes.to_bytes(4, 'big'))
+        
+        # Segunda passada:
+        trie = No(string, 0)
+        cod = 1
+        counter = 0
+        while True:
+            char = fEntrada.read(1)
+            if not char:
+                break
+            busca = trie.busca(string + char)
+            if busca != None:
+                string = string + char
+            else:
+                counter += 1
+                busca2 = trie.busca(string)
+                print([busca2.to_bytes(bytes, 'big'), char])
+                fSaida.write(busca2.to_bytes(bytes, 'big'))
+                fSaida.write(char.encode('utf-8'))
+                trie.addPalavra(string + char, cod)
+                cod += 1
+                string = ''
+
+fEntrada = open("teste.txt")
+fSaida = open("testezip.zip", 'wb')
+compressor = lz78
+compressor.comprime(fEntrada, fSaida)
